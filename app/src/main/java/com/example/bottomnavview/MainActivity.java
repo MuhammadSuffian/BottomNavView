793cb4a762f1bb8e645cfb505499ccbbf194b6c4
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2024-12-10 10:32:46 +0500

    v1

--- app/src/main/java/com/example/bottomnavview/MainActivity.java
+++ app/src/main/java/com/example/bottomnavview/MainActivity.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.bottomnavview.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.drawer.setOnClickListener(v->{
+            binding.maindrawer.openDrawer(GravityCompat.START);
+        });
     }
 }
